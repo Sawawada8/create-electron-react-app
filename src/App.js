4135ee677ec2e2ebb6c88f27ca82c64d7cb2af6c
sawada <sawada@csplan.jp>
2021-08-24 07:13:19 +0900

    setup express

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {
+    useState, 
+    useEffect,
+    useRef
+} from 'react'
+import axios from 'axios'
 
 function App() {
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        axios.get('http://localhost:3333')
+            .then((val) => {
+                console.log(val.data)
+                setText(val.data.data)
+            })
+    }, [])
+
+    const connetApi = () => {
+        axios.get('http://localhost:3333')
+            .then((val) => {
+                setText('::api::' + val.data.data + '::api::')
+            })
+    }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +33,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> {text} </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +42,17 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => setText(text + "add!")}>hello</button>
+        <button onClick={connetApi}>api</button>
       </header>
+        <button onClick={() => {
+            if (true) {
+                console.log('htl')
+            }
+        }}></button>
     </div>
   );
 }
 
 export default App;
+
